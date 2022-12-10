7d7651f6283dfc5ec1ce405b74de6eab3c01fcd8
Luis Hernández <53943021+luishernand@users.noreply.github.com>
2022-12-10 13:17:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 #--------------------------#
 import streamlit as st 
 import pandas as pd 
-import plotly.express as px 
+#import plotly.express as px 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
